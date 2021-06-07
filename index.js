6225439987368119c4fd843723ad8814466b1b45
Ramadhan Hadiatma <nandaaprilia605@gmail.com>
2021-06-07 19:49:15 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const csvtojson = require('csvtojson')
+const fs = require('fs')
+
+const csvfilepath = "simple.csv"
+
+csvtojson()
+.fromFile(csvfilepath)
+.then((json) => {
+    console.log(json)
+
+    fs.writeFileSync("output.json",JSON.stringify(json),"utf-8",(err) => {
+        if(err) console.log(err)
+    })
+})
